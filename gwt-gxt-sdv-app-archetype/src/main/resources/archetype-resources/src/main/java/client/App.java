081a75cff43ad2b56b3b5dfc0951d5875a1d3922
Piotr Kosmowski <kospiotr@gmail.pl>
2014-02-08 12:13:31 +0100

    Names refactoring and add archetypes

---
+++ gwt-gxt-sdv-app-archetype/src/main/resources/archetype-resources/src/main/java/client/App.java
@@ -0,0 +1,16 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.sencha.gxt.widget.core.client.button.TextButton;
+
+public class App implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        RootPanel.get().add(new TextButton("TextButton"));
+    }
+}
